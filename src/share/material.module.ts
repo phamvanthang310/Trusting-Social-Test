f9ed04d54d62cbb21606fde2fdf75a1965c86c21
thangpham <phamvanthang310@gmail.com>
2018-01-29 02:50:55 +0700

    Update header, footer style

--- src/share/material.module.ts
+++ src/share/material.module.ts
@@ -4,7 +4,8 @@ import {
   MatCardModule,
   MatIconModule,
   MatProgressSpinnerModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatTooltipModule
 } from '@angular/material';
 
 @NgModule({
@@ -14,6 +15,7 @@ import {
     MatCardModule,
     MatButtonModule,
     MatProgressSpinnerModule,
+    MatTooltipModule,
   ],
   exports: [
     MatToolbarModule,
@@ -21,6 +23,7 @@ import {
     MatCardModule,
     MatButtonModule,
     MatProgressSpinnerModule,
+    MatTooltipModule,
   ]
 })
 export class MaterialModule {
