dc0a6978a4ad597857d73dd13a1efd9b45832d85
thangpham <phamvanthang310@gmail.com>
2018-01-31 00:25:09 +0700

    Implement e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,36 @@
-import { AppPage } from './app.po';
+import { browser, by, element } from 'protractor';
 
 describe('trusting-social-test App', () => {
-  let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
+  beforeAll(() => {
+    browser.get('/');
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it(`should navigate to '/news' by default`, () => {
+    expect(browser.getCurrentUrl()).toMatch('/news');
+    expect(browser.getTitle()).toEqual('Trusting Social - The New York Times');
+  });
+
+  it('should display 10 new items when page is first loaded', () => {
+    const newsItems = element.all(by.css('.news__container'));
+    expect(newsItems.count()).toEqual(10);
+  });
+
+  it('should display 20 news when clicking load more button', () => {
+    const loadMoreBtn = element(by.css('.news__load-more button'));
+    loadMoreBtn.click();
+
+    const newsItems = element.all(by.css('.news__container'));
+    expect(newsItems.count()).toEqual(20);
+  });
+
+  it('should open dialog details when clicking news item', () => {
+    const dialog = element(by.css('.mat-dialog-container'));
+    expect(dialog.isPresent()).toBeFalsy();
+
+    const newsItem = element.all(by.css('.news__container')).first();
+    newsItem.click();
+
+    expect(dialog.isPresent()).toBeTruthy();
   });
 });
